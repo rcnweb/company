d9f2cc423f0110de5ea2b7f923097b34019718c5
Rodrigo Fonseca <rodrigo.fonseca@crea-rj.org.br>
2018-12-05 18:03:43 -0200

    continuacao

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Site from './Site'
+import Rodape from './componentes/Rodape'
+import Cabecalho from './componentes/Cabecalho'
+import Inicio from './componentes/Inicio'
+import Servicos from './componentes/Servicos'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Cabecalho />
+        <Inicio />
+        <Servicos />
+        <Site />
+        <Rodape />
       </div>
     );
   }
